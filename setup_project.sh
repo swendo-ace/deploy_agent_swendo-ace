b9cffc5592dd9ea4d40cd5c31a1937705263e990
swendo-ace <s.wendo@alustudent.com>
2026-06-13 13:32:11 -0700

    Add directory structure creation

--- setup_project.sh
+++ setup_project.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "setup_project.sh"
-read -p "What would you like to name your folder?" foldername
+echo "Welcome to the attendance_tracker!"
+read -p "What would you like to name your folder? " foldername
 echo "Hello, $foldername!"
-
+mkdir -p "attendance_tracker_$foldername" "attendance_tracker_$foldername/Helpers" "attendance_tracker_$foldername/reports"
